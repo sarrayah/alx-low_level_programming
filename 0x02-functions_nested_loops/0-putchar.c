c88b39dcf70046436bf209f484146eabbb30586c
Sarah <tiiwatope@gmail.com>
2023-02-22 16:15:50 -0800

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*main-print _putchar
+*followed by a new line
+*Return: Always 0
+*/
+
+int main(void)
+{
+	char text[8] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
