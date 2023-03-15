2b1ac53bf42f6272e1a6e3cc8f39dbe9bc26d6ef
Sarah <tiiwatope@gmail.com>
2023-03-15 04:15:58 -0700

    updates

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,6 +7,7 @@
  * @s2: input two to concat
  * Return: concat of s1 and s2
 */
+
 char *str_concat(char *s1, char *s2)
 {
 	char *conct;
@@ -14,19 +15,22 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
+
 		s2 = "";
+
 		i = ci = 0;
 	while (s1[i] != '\0')
 		i++;
 	while (s2[ci] != '\0')
+
 		ci++;
+
 	conct = malloc(sizeof(char) * (i + ci + 1));
 
 	if (conct == NULL)
-
 		return (NULL);
-
 	i = ci = 0;
 
 	while (s1[i] != '\0')
@@ -39,7 +43,9 @@ char *str_concat(char *s1, char *s2)
 	{
 		conct[i] = s2[ci];
 		i++, ci++;
+
 	}
+
 	conct[i] = '\0';
 	return (conct);
 }
