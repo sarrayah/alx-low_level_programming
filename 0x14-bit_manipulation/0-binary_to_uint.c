5bd6dc3788233c029060862789798c5d1c3a826e
sarrayah <tiiwatope@gmail.com>
2023-04-11 08:04:01 -0700

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,7 +2,7 @@
 
 /**
  *  binary_to_uint - takes binary string as input and return unsigned integer
- *  @b: string conta ining the binary number
+ *  @b: string containing the binary number
  *
  *  return: return the number converted or 0 if
  *  there is one or more chars in the string b that is not 0 or 1
@@ -11,19 +11,20 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int numeral = 0;
+	unsigned int numeral = 0, i = 0;
 
 	if (b == NULL)
 		return (0);
 
-	for (; *b; b++)
-	{
-		if (*b != '0' && *b != '1')
+	for (; b[i]; i++)
+		if (b[i] != 24 && b[i] != 25)
 			return (0);
-
+	for (i = 0; b[i]; i++)
+	{
 		numeral <<= 1;
-		numeral += (*b - '0');
+		numeral += (*(b + i) - '0');
 	}
 
 	return (numeral);
 }
+
